59a3000ac0af6e6eeaff52191ab8f384f9483dc8
philip82148 <92205270+philip82148@users.noreply.github.com>
2023-05-06 12:03:52 +0900

    separate as a submodule from kishukusha-meibo

---
+++ csv.js
@@ -0,0 +1,60 @@
+async function csvToArray(file) {
+  const content = await new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.readAsText(file);
+  });
+
+  return csvStringToArray(content);
+}
+
+function csvStringToArray(csvString) {
+  return [...getRowStrings(csvString)].map((rowString) => [
+    ...getCells(rowString),
+  ]);
+}
+
+function* getRowStrings(csvString) {
+  const lines = csvString.split(/\r?\n/);
+
+  let row = [];
+  let quotCount = 0;
+  for (const line of lines) {
+    row.push(line);
+    quotCount += (line.match(/"/g) || []).length;
+
+    if (quotCount % 2) continue;
+
+    const csvRowString = row.join("\r\n");
+    if (csvRowString) yield csvRowString;
+
+    row = [];
+    quotCount = 0;
+  }
+}
+
+function* getCells(csvRowString) {
+  const cells = csvRowString.split(/,/);
+
+  let cell = [];
+  let quotCount = 0;
+  for (const line of cells) {
+    cell.push(line);
+    quotCount += (line.match(/"/g) || []).length;
+
+    if (quotCount % 2) continue;
+
+    const cellString = cell.join(",");
+    yield cellString.replace(/^"|""|"$/g, function (match) {
+      return {
+        '"': "",
+        '""': '"',
+      }[match];
+    });
+
+    cell = [];
+    quotCount = 0;
+  }
+}
