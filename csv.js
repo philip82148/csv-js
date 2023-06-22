1d179b71ee3d4aa8f81a68f2825695a9fb698101
philip82148 <92205270+philip82148@users.noreply.github.com>
2023-06-23 02:09:45 +0900

    fix lint

--- csv.js
+++ csv.js
@@ -1,65 +1,63 @@
 export async function csvFileToArray(csvFile) {
   const csvString = await new Promise((resolve) => {
-    const reader = new FileReader();
+    const reader = new FileReader()
     reader.onload = () => {
-      resolve(reader.result);
-    };
-    reader.readAsText(csvFile);
-  });
+      resolve(reader.result)
+    }
+    reader.readAsText(csvFile)
+  })
 
-  return csvStringToArray(csvString);
+  return csvStringToArray(csvString)
 }
 
 export async function csvUrlToArray(csvUrl) {
-  const csvString = await fetch(csvUrl).then((response) => response.text());
-  return csvStringToArray(csvString);
+  const csvString = await fetch(csvUrl).then((response) => response.text())
+  return csvStringToArray(csvString)
 }
 
 export function csvStringToArray(csvString) {
-  return [...getCsvRowStrings(csvString)].map((csvRowString) => [
-    ...getCellStrings(csvRowString),
-  ]);
+  return [...getCsvRowStrings(csvString)].map((csvRowString) => [...getCellStrings(csvRowString)])
 }
 
 function* getCsvRowStrings(csvString) {
-  const lines = csvString.split(/\r?\n/);
+  const lines = csvString.split(/\r?\n/)
 
-  let row = [];
-  let quotCount = 0;
+  let row = []
+  let quotCount = 0
   for (const line of lines) {
-    row.push(line);
-    quotCount += (line.match(/"/g) || []).length;
+    row.push(line)
+    quotCount += (line.match(/"/g) || []).length
 
-    if (quotCount % 2) continue;
+    if (quotCount % 2) continue
 
-    const csvRowString = row.join("\r\n");
-    if (csvRowString) yield csvRowString;
+    const csvRowString = row.join('\r\n')
+    if (csvRowString) yield csvRowString
 
-    row = [];
-    quotCount = 0;
+    row = []
+    quotCount = 0
   }
 }
 
 function* getCellStrings(csvRowString) {
-  const cells = csvRowString.split(/,/);
+  const cells = csvRowString.split(/,/)
 
-  let cell = [];
-  let quotCount = 0;
+  let cell = []
+  let quotCount = 0
   for (const line of cells) {
-    cell.push(line);
-    quotCount += (line.match(/"/g) || []).length;
+    cell.push(line)
+    quotCount += (line.match(/"/g) || []).length
 
-    if (quotCount % 2) continue;
+    if (quotCount % 2) continue
 
-    const cellString = cell.join(",").replace(/^"|""|"$/g, function (match) {
+    const cellString = cell.join(',').replace(/^"|""|"$/g, function (match) {
       return {
-        '"': "",
+        '"': '',
         '""': '"',
-      }[match];
-    });
-    yield cellString;
+      }[match]
+    })
+    yield cellString
 
-    cell = [];
-    quotCount = 0;
+    cell = []
+    quotCount = 0
   }
 }
